013eab4172eaf59454cb39eb704775d646bbc787
OOKucherenko <okld52dec@gmail.com>
2023-01-23 23:16:16 +0200

    фыав

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// import c from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+// Change code below this line
+// console.log(galleryItems);
+// const containerGallery = document.querySelector('.gallery');
+// const imgMarkup = createImages(galleryItems);
+// containerGallery.insertAdjacentHTML('beforeend', imgMarkup);
 
-console.log(galleryItems);
+// function createImages(galleryItems) {
+//   return galleryItems
+//     .map(({ original, preview, description }) => {
+//       return `
+// <a class="gallery__item" href="${original}">
+//   <img class="gallery__image" src="${preview}" alt="${description}" />
+// </a>`;
+//     })
+//     .join('');
+// }
+
+// let gallery = new SimpleLightbox('.gallery a', {
+//   captionsData: 'alt',
+//   captionDelay: '250',
+// });
