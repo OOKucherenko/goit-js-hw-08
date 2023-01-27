be055a189f57d0267af4ef4ba0b04fd10d892b2f
OOKucherenko <okld52dec@gmail.com>
2023-01-28 00:55:20 +0200

    add

---
+++ src/js/03-feedback.js
@@ -0,0 +1,42 @@
+const throttle = require('lodash.throttle');
+const refs = {
+  input: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
+  formSubmit: document.querySelector('form'),
+  LOCALSTORAGE_KEY: 'feedback-form-state',
+};
+
+refs.input.value =
+  JSON.parse(localStorage.getItem(refs.LOCALSTORAGE_KEY))?.inputValue || '';
+
+refs.textarea.value =
+  JSON.parse(localStorage.getItem(refs.LOCALSTORAGE_KEY))?.textareaValue || '';
+
+const form = {
+  inputValue:
+    JSON.parse(localStorage.getItem(refs.LOCALSTORAGE_KEY))?.inputValue || '',
+  textareaValue:
+    JSON.parse(localStorage.getItem(refs.LOCALSTORAGE_KEY))?.textareaValue ||
+    '',
+};
+
+const handleInput = e => {
+  form.inputValue = e.target.value;
+  localStorage.setItem(refs.LOCALSTORAGE_KEY, JSON.stringify(form));
+};
+
+refs.input.addEventListener('input', throttle(handleInput, 500));
+
+const handleTextarea = e => {
+  form.textareaValue = e.target.value;
+  localStorage.setItem(refs.LOCALSTORAGE_KEY, JSON.stringify(form));
+};
+
+refs.textarea.addEventListener('input', throttle(handleTextarea, 500));
+
+refs.formSubmit.addEventListener('submit', e => {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(refs.LOCALSTORAGE_KEY);
+  console.log(form);
+});
